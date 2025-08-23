4d45f7fbfb2e4df7fd251a8e5a85304c5c8172ec
Christian Elsen <chris@chriselsen.net>
2025-08-23 07:01:25 +0000

    Add blue highlights to all CoT unit selection tabs
    - Add text-blue class to Coordinate component tabs (DD, DMS, MGRS, UTM)
    - Add text-blue class to PropertyBearing component tabs (Deg, Rads, Mils)
    - Complete blue highlight implementation for all unit selection components:
      - PropertySpeed (KM/H, MPH, M/S)
      - LineLength (Kilometers, Meters, Miles, Yards, Feet)
      - PolygonArea (Feet², Meters², Acres, Ha)
      - Coordinate (DD, DMS, MGRS, UTM)
      - PropertyBearing (Deg, Rads, Mils)

--- api/web/src/components/CloudTAK/util/PropertySpeed.vue
+++ api/web/src/components/CloudTAK/util/PropertySpeed.vue
@@ -13,22 +13,22 @@
                 :size='24'
             />
             <span
-                v-tooltip='"Meters Per Second"'
+                v-tooltip='"Kilometers Per Hour"'
                 class='my-1 px-2 user-select-none'
                 :class='{
-                    "bg-gray-500 rounded-bottom": mode === "m/s",
-                    "cursor-pointer": mode !== "m/s",
+                    "bg-gray-500 rounded-bottom text-blue": mode === "km/h",
+                    "cursor-pointer": mode !== "km/h",
                 }'
                 role='menuitem'
                 tabindex='0'
-                @keyup.enter='mode = "m/s"'
-                @click='mode = "m/s"'
-            >M/S</span>
+                @keyup.enter='mode = "km/h"'
+                @click='mode = "km/h"'
+            >KM/H</span>
             <span
                 v-tooltip='"Miles Per Hour"'
                 class='my-1 px-2 user-select-none'
                 :class='{
-                    "bg-gray-500 rounded-bottom": mode === "mi/h",
+                    "bg-gray-500 rounded-bottom text-blue": mode === "mi/h",
                     "cursor-pointer": mode !== "mi/h",
                 }'
                 role='menuitem'
@@ -37,17 +37,17 @@
                 @click='mode = "mi/h"'
             >MPH</span>
             <span
-                v-tooltip='"Kilometers Per Hour"'
+                v-tooltip='"Meters Per Second"'
                 class='my-1 px-2 user-select-none'
                 :class='{
-                    "bg-gray-500 rounded-bottom": mode === "km/h",
-                    "cursor-pointer": mode !== "km/h",
+                    "bg-gray-500 rounded-bottom text-blue": mode === "m/s",
+                    "cursor-pointer": mode !== "m/s",
                 }'
                 role='menuitem'
                 tabindex='0'
-                @keyup.enter='mode = "km/h"'
-                @click='mode = "km/h"'
-            >KM/H</span>
+                @keyup.enter='mode = "m/s"'
+                @click='mode = "m/s"'
+            >M/S</span>
         </div>
     </div>
 </template>
